7ef0c53274da03d534bf97a62b7adbb6e93f8410
Kerwin <1078933746@qq.com>
2021-01-09 14:19:46 +0800

    完成了商品列表页面

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		
+		<view class="good-list">
+			<view v-for="(goods, index) in goodsList" :key="index" @click="gotoDetail(goods)">
+				<my-goods :goods="goods"></my-goods>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +12,71 @@
 	export default {
 		data() {
 			return {
-				
+				// 请求参数对象
+				queryObj: {
+					// 查询关键词
+					query: '',
+					// 商品分类Id
+					cid: '',
+					// 页码值
+					pagenum: 1,
+					// 每页显示多少条数据
+					pagesize: 10
+				},
+				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png',
+				isloading: false,
+				goodsList: [],
+				total: 0
 			};
+		},
+		onLoad(options) {
+			console.log(options)
+			// 将页面参数转存到 this.queryObj 对象中
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+
+			this.getGoodsList()
+		},
+		// 触底的事件
+		onReachBottom() {
+			// 判断是否还有下一页数据
+			if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+			if (this.isloading) return
+			// 让页码值自增 +1
+			this.queryObj.pagenum += 1
+			// 重新获取列表数据
+			this.getGoodsList()
+		},
+		// 上拉刷新
+		onPullDownRefresh() {
+			// 1. 重置关键数据
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isloading = false
+			this.goodsList = []
+
+			this.getGoodsList(() => uni.stopPullDownRefresh())
+		},
+		methods: {
+			async getGoodsList(cb) {
+				this.isloading = true
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				this.isloading = false
+				cb && cb()
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.goodsList = [...this.goodsList, ...res.message.goods]
+				this.total = res.message.total
+			},
+			gotoDetail(id) {
+				uni.navigateTo({
+					url: '/subpkg/goods_detail/goods_detail?goods_id=' + id.goods_id
+				})
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
-
 </style>
